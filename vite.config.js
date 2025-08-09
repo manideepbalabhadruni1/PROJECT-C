7d4373cdd68aa89179849b3d4380dc974e0c0e0f
unknown <manideepbalabhadruni4@gmail.com>
2025-08-09 17:10:43 +0530

    i1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PROJECT/', // Change "PROJECT" to your actual repo name if deploying to GitHub Pages
+  base: '/PROJECT-C/',
 });
